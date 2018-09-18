bcaa686602b4bcb024c74e2f5fecd27d7f16d5ff
Toshihiro Arae <aroearue@gmail.com>
2018-09-18 16:06:46 +0900

    modify bug

--- tests/testthat/test-fasta-io.R
+++ tests/testthat/test-fasta-io.R
@@ -1,6 +1,6 @@
 context("test-fasta-io")
 
-test_file_path <- system.file("extdata", "aa_test.fas", package = "bioseqr")
+test_file_path <- system.file("extdata", "aa_test.fas", package = "bstringr")
 # write_fasta(bstr("This is test.", "TEST1", F), test_file_path)
 
 test_that("read_fasta()", {
