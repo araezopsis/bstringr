dff6250a6e9b5ee0e7c8b333c85c313a2a6109b9
Toshihiro Arae <aroearue@gmail.com>
2018-09-22 16:25:45 +0900

    feat: bstr_sub_true

--- R/bstr-methods.R
+++ R/bstr-methods.R
@@ -12,6 +12,40 @@ bstr_sub <-
 
     bstrobj <- str_sub(bstrobj, start = start, end = end)
 
+    attributes(bstrobj) <- at
+    bstrobj
+  }
+
+#' bstr_sub_true
+#' @importFrom stringr str_sub
+#' @inheritParams bstr_sub
+#' @param i index
+#' @param gap_chr gap character
+#' @export
+bstr_sub_true <-
+  function(bstrobj, i, start, end, gap_chr = "-"){
+    bstrobj <- as_bstr(bstrobj)
+    at <- attributes(bstrobj)
+
+    tosub <- bstrobj[i]
+    if(length(tosub) != 1) stop("index i is wrong.")
+
+    tosub <-
+      str_extract_all(tosub, ".")[[1]] %>%
+      str_detect(paste0("[^", gap_chr, "]")) %>%
+      cumsum
+
+    if(missing(start)) start <- 1
+    if(missing(end)) end <- max(tosub)
+
+    bstrobj <-
+      str_sub(
+        bstrobj,
+        start = which(tosub == start)[1],
+        end = which(tosub == end)[1]
+      )
+
+    attributes(bstrobj) <- at
     bstrobj
   }
 
