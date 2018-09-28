9a58938e6c8f19db50e03c2f190a57b2b3f16646
Toshihiro Arae <aroearue@gmail.com>
2018-09-28 15:29:45 +0900

    add: class_astr.R

---
+++ R/class-astr.R
@@ -0,0 +1,43 @@
+
+AA_ALPHABET <-
+  c("A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K",
+    "M", "F", "P", "S", "T", "W", "Y", "V")
+
+# AA_ALPHABET %>% stringr::str_c(collapse = "") %>% paste0("[", ., "]")
+AA_ALPHABET_REGEX <-
+  "[ARNDCQEGHILKMFPSTWYV\\-\\+\\.\\*]"
+AA_ALPHABET_REGEX_not <-
+  "[^ARNDCQEGHILKMFPSTWYV\\-\\+\\.\\*]"
+
+#' Constructing astr class object
+#' @importFrom stringr str_detect
+#' @importFrom stringr str_to_upper
+#' @param x Character vector
+#' @param n Character vector
+#' @param ucase Character vector
+#' @export
+astr <-
+  function(x, n, ucase = T){
+    a <- bstr(x, n, ucase)
+    if(any(str_detect(str_to_upper(a), AA_ALPHABET_REGEX_not)))
+      stop("input contains NOT Amino Acid character")
+    class(a) <- c("astr", class(a))
+    a
+  }
+
+#' check class
+#' @param x x
+#' @export
+is_astr <- function(x) inherits(x, "astr")
+
+#' Convert character vector to dstr class
+#' @inheritParams bstr
+#' @export
+as_astr <-
+  function(x, n){
+    if(!is_astr(x)){
+      return(astr(x, n))
+    }else{
+      x
+    }
+  }
