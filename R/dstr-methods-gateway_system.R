825cef61226bbef988de0cb2f6f42616ac9270a0
Toshihiro Arae <aroearue@gmail.com>
2019-12-27 09:11:54 +0900

    dstr-methods-gateway_system.R
    関数呼び出しを変更

--- R/dstr-methods-gateway_system.R
+++ R/dstr-methods-gateway_system.R
@@ -1,11 +1,5 @@
 
 #' Return the product sequence of after TOPO reaction
-#' @importFrom stringr str_detect
-#' @importFrom stringr str_extract
-#' @importFrom stringr str_to_lower
-#' @importFrom stringr str_to_upper
-#' @importFrom stringr str_c
-#' @importFrom purrr pmap
 #' @param cds coding sequence which you want to clone. without 5'CACC
 #' @export
 dstr_TOPO <-
@@ -19,13 +13,15 @@ dstr_TOPO <-
     rrec_out <- "AAGGGTGGGCGCGCCGAC"
 
     result_seq <-
-      pmap(
+      purrr::pmap(
         list(
-          str_extract(entry, paste0("^.*", lrec_out)) %>% str_to_lower(),
-          cds %>% str_to_upper(),
-          str_extract(entry, paste0(rrec_out, ".*$")) %>% str_to_lower()
+          stringr::str_extract(entry, paste0("^.*", lrec_out)) %>%
+            stringr::str_to_lower(),
+          cds %>% stringr::str_to_upper(),
+          stringr::str_extract(entry, paste0(rrec_out, ".*$")) %>%
+            stringr::str_to_lower()
         ),
-        ~ str_c(..1, ..2, ..3)
+        ~ stringr::str_c(..1, ..2, ..3)
       )
 
     result_seq <- dstr(unlist(result_seq), n, ucase = F)
@@ -35,14 +31,6 @@ dstr_TOPO <-
 
 
 #' Return product sequence after LR reaction
-#' @importFrom stringr str_detect
-#' @importFrom stringr str_extract
-#' @importFrom stringr str_replace
-#' @importFrom stringr str_to_upper
-#' @importFrom stringr str_to_lower
-#' @importFrom stringr str_sub
-#' @importFrom stringr str_count
-#' @importFrom magrittr %>%
 #' @param entry Entry clone
 #' @param distination Distination vector
 #' @export
@@ -59,28 +47,28 @@ dstr_LR <-
     lrec_in  <- "(?i)CCAACTTTGTACAAAAAAGCAGGCT" #attL1
     rrec_in  <- "(?i)ACCCAGCTTTCTTGTACAAAGTTGG" #attL2
 
-    if(any(!str_detect(distination, lrec_out))) stop("outer have no lrec_out sequence.")
-    if(any(!str_detect(distination, rrec_out))) stop("outer have no rrec_out sequence.")
-    if(any(!str_detect(entry, lrec_in))) stop("insert have no lrec_in sequence.")
-    if(any(!str_detect(entry, rrec_in))) stop("insert have no rrec_in sequence.")
+    if(any(!stringr::str_detect(distination, lrec_out))) stop("outer have no lrec_out sequence.")
+    if(any(!stringr::str_detect(distination, rrec_out))) stop("outer have no rrec_out sequence.")
+    if(any(!stringr::str_detect(entry, lrec_in))) stop("insert have no lrec_in sequence.")
+    if(any(!stringr::str_detect(entry, rrec_in))) stop("insert have no rrec_in sequence.")
 
     recombinate <-
       paste0(
         #outer_left
-        str_to_lower(
-          str_extract(distination, paste0("^.*", lrec_out)) %>%
-          {str_sub(., end = str_count(.) - 17)}
+        stringr::str_to_lower(
+          stringr::str_extract(distination, paste0("^.*", lrec_out)) %>%
+          {stringr::str_sub(., end = stringr::str_count(.) - 17)}
         ),
         #insert
-        str_to_upper(
-          str_extract(entry, paste0(lrec_in, ".*", rrec_in)) %>%
-          {str_sub(., start = 9, end = str_count(.) - 15)}
+        stringr::str_to_upper(
+          stringr::str_extract(entry, paste0(lrec_in, ".*", rrec_in)) %>%
+          {stringr::str_sub(., start = 9, end = stringr::str_count(.) - 15)}
         ),
 
         #outer_right
-        str_to_lower(
-          str_extract(distination, paste0(rrec_out, ".*$")) %>%
-            str_sub(start = 11)
+        stringr::str_to_lower(
+          stringr::str_extract(distination, paste0(rrec_out, ".*$")) %>%
+            stringr::str_sub(start = 11)
         )
       )
     recombinate %>% dstr(ne)
@@ -98,13 +86,6 @@ dstr_TOPO_LR <-
 
 
 #' Find a main open reading frame from Gataway system vector
-#' @importFrom stringr str_locate_all
-#' @importFrom stringr str_sub
-#' @importFrom stringr str_detect
-#' @importFrom purrr map
-#' @importFrom purrr map2
-#' @importFrom purrr map_int
-#' @importFrom purrr pmap_chr
 #' @param x a vector sequence you want to find orfs
 #' @export
 dstr_find_orf_from_vector <-
@@ -116,26 +97,33 @@ dstr_find_orf_from_vector <-
     attB1 <- "ACAAGTTTGTACAAAAAAGCAGGCT"
     attB2 <- "ACCCAGCTTTCTTGTACAAAGTGGT"
 
-    pos_start <- str_locate_all(dstrobj, attB1) %>% map(~ .x[,2])
-    pos_end <- str_locate_all(dstrobj, attB2) %>% map(~ .x[,1])
+    pos_start <-
+      stringr::str_locate_all(dstrobj, attB1) %>%
+      purrr::map(~ .x[,2])
+    pos_end <-
+      stringr::str_locate_all(dstrobj, attB2) %>%
+      purrr::map(~ .x[,1])
 
-    if(any(map_int(pos_start, length) != 1L)) stop("Number of attB1 is wrong.")
-    if(any(map_int(pos_end, length) != 1L)) stop("Number of attB2 is wrong.")
+    if(any(purrr::map_int(pos_start, length) != 1L))
+      stop("Number of attB1 is wrong.")
+    if(any(purrr::map_int(pos_end, length) != 1L))
+      stop("Number of attB2 is wrong.")
 
     B1toB2 <-
-      pmap_chr(
+      purrr::pmap_chr(
         list(dstrobj, pos_start, pos_end),
-        ~ str_sub(..1, ..2 + 21, ..3 - 17)
+        ~ stringr::str_sub(..1, ..2 + 21, ..3 - 17)
       )
-    if(any(map_int(B1toB2, nchar) == 0)) stop("Inserted sequence of pENTR D-TOPO is missing.")
+    if(any(purrr::map_int(B1toB2, nchar) == 0))
+      stop("Inserted sequence of pENTR D-TOPO is missing.")
 
     orf_li <- dstr_find_orf(dstrobj)
     main_orf <-
-      map2(orf_li, B1toB2,
+      purrr::map2(orf_li, B1toB2,
            ~ list(
              "seq_name" = .x$seq_name,
-             "orf" = .x$orf[str_detect(.x$orf, .y)])) %>%
-      map(~ dstr(sort(.x$orf, T, by = "l")[1], paste(.x$seq_name, "ORF")))
+             "orf" = .x$orf[stringr::str_detect(.x$orf, .y)])) %>%
+      purrr::map(~ dstr(sort(.x$orf, T, by = "l")[1], paste(.x$seq_name, "ORF")))
     main_orf %>% unlist %>% dstr
   }
 
