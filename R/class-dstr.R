b14660875f36b07f2011660eced9e63453bf7513
Toshihiro Arae <aroearue@gmail.com>
2019-12-27 17:01:32 +0900

    Update class-dstr.R
    examples追加

--- R/class-dstr.R
+++ R/class-dstr.R
@@ -2,8 +2,12 @@
 #' Constructer of the dstr class object
 #' @inheritParams class_bstr_arg
 #' @export
+#' @examples
+#' dstr("A.Bad.bat", "It is")
+#' dstr(c("A", "bad", "Bat"), ucase = TRUE)
+#'
 dstr <-
-  function(x, n, ucase = F){
+  function(x, n, ucase = FALSE){
     d <- bstr(x, n, ucase)
     if(any(is_valid_dna_character(d, negate = TRUE)))
       stop("input contains invalid DNA character")
@@ -15,6 +19,12 @@ dstr <-
 #' @inheritParams class_bstr_arg
 #' @param negate If TRUE, return non-matching elements.
 #' @export
+#' @examples
+#' test <- c("a", "B", "c", "D", "e")
+#' is_valid_dna_character(test)
+#' is_valid_dna_character(test, negate = TRUE) %>%
+#'   test[.]
+#'
 is_valid_dna_character <-
   function(x, negate = FALSE) {
     if(!is.character(x)) stop("x must be a character vector")
@@ -25,13 +35,20 @@ is_valid_dna_character <-
 #' check dstr class
 #' @inheritParams class_bstr_arg
 #' @export
+#' @examples
+#' is_dstr(dstr("bad"))
+#'
 is_dstr <- function(x) inherits(x, "dstr")
 
 #' Convert character vector to dstr class
 #' @inheritParams class_bstr_arg
 #' @export
+#' @examples
+#' as_dstr("bad", "good", ucase = TRUE)
+#' # as_dstr(c("good", "bad")) # Error
+#'
 as_dstr <-
-  function(x, n, ucase = F){
+  function(x, n, ucase = FALSE){
     if(!is_dstr(x)){
       return(dstr(x, n, ucase))
     }else{
