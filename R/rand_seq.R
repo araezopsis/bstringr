924850ad18f20c73aeaccb21c4418ef3f5178abb
Toshihiro Arae <aroearue@gmail.com>
2020-01-08 12:35:38 +0900

    Update rand_seq.R
    seed引数を追加

--- R/rand_seq.R
+++ R/rand_seq.R
@@ -3,25 +3,32 @@
 #' @param n single integer, nubmer of sequences
 #' @param length integer vector, desired sequence lengths
 #' @param pattern character vector specifying character classes
+#' @param seed a single integer. specify seeds.
 #' @export
 #' @examples
-#' bstr_rand_seq(3, 20)
-#' bstr_rand_seq(3, 5:7, c("[a-c]", "[A-C]", "[1-3]"))
+#' bstr_rand_seq(3, 20, seed = 1)
+#' bstr_rand_seq(3, 5:7, c("[a-c]", "[A-C]", "[1-3]"), seed = 1)
 #'
-bstr_rand_seq <- function(n, length, pattern = "[A-Za-z]") {
-    stringi::stri_rand_strings(n = n, length = length, pattern = pattern) %>%
-      as_bstr()
+bstr_rand_seq <- function(n, length, pattern = "[A-Za-z]", seed) {
+  if(!missing(seed)) {
+    set.seed(seed)
   }
+  stringi::stri_rand_strings(n = n, length = length, pattern = pattern) %>%
+    as_bstr()
+}
 
 #' Generate random dstr sequences of desired lengths.
 #' @inheritParams bstr_rand_seq
 #' @export
 #' @examples
-#' dstr_rand_seq(3, 20)
-#' dstr_rand_seq(3, 5:7, c("[atcg]", "[ATCG]", "[N.]"))
+#' dstr_rand_seq(3, 20, seed = 1)
+#' dstr_rand_seq(3, 5:7, c("[atcg]", "[ATCG]", "[N.]"), seed = 1)
 #'
 dstr_rand_seq <-
-  function(n, length, pattern = "[ATGC]") {
+  function(n, length, pattern = "[ATGC]", seed) {
+    if(!missing(seed)) {
+      set.seed(seed)
+    }
     stringi::stri_rand_strings(n = n, length = length, pattern = pattern) %>%
       as_dstr()
   }
@@ -30,11 +37,14 @@ dstr_rand_seq <-
 #' @inheritParams bstr_rand_seq
 #' @export
 #' @examples
-#' astr_rand_seq(3, 20)
-#' astr_rand_seq(3, 5:7, c("[arnd]", "[ARND]", "[X.]"))
+#' astr_rand_seq(3, 20, seed = 1)
+#' astr_rand_seq(3, 5:7, c("[arnd]", "[ARND]", "[X.]"), seed = 1)
 #'
 astr_rand_seq <-
-  function(n, length, pattern = "[ARNDCQEGHILKMFPSTWYVX]") {
+  function(n, length, pattern = "[ARNDCQEGHILKMFPSTWYVX]", seed) {
+    if(!missing(seed)) {
+      set.seed(seed)
+    }
     stringi::stri_rand_strings(n = n, length = length, pattern = pattern) %>%
       as_astr()
   }
