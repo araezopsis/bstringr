a54815b275a9d44b8e76bbc2087bb8313c4eb1af
Toshihiro Arae <aroearue@gmail.com>
2020-01-09 16:58:31 +0900

    Update bstr_length.R bstr_reverse.R

--- R/bstr_length.R
+++ R/bstr_length.R
@@ -2,10 +2,22 @@
 #' Sequence length in the bstr sequences.
 #' @inheritParams class_bstr
 #' @export
+#' @name length
 #' @examples
-#' bstr_length(c("ATG", "cc tgAGT2--"))
+#' test <- c("ATG", "cc tgAGT2--")
+#' bstr_length(test)
 #' bstr_length(NA_character_)
 #'
-bstr_length <- function(bstrobj) {
-  as_bstr(bstrobj) %>% stringr::str_length()
-}
+bstr_length <-
+  function(bstrobj) {
+    as_bstr(bstrobj) %>% stringr::str_length()
+  }
+
+#' @rdname length
+#' @export
+dstr_length <- bstr_length
+
+#' @rdname length
+#' @export
+astr_length <- bstr_length
+
--- R/bstr_reverse.R
+++ R/bstr_reverse.R
@@ -2,8 +2,9 @@
 #' Reverse bstr sequence
 #' @inheritParams class_bstr
 #' @export
+#' @name reverse
 #' @examples
-#' temp <- bstr_rand_seq(2, 6)
+#' temp <- bstr_rand_seq(2, 6, seed = 1)
 #' c(temp, reverse = bstr_reverse(temp))
 #'
 bstr_reverse <-
@@ -16,3 +17,12 @@ bstr_reverse <-
     attributes(bstrobj) <- at
     bstrobj
   }
+
+#' @rdname reverse
+#' @export
+dstr_reverse <- bstr_reverse
+
+#' @rdname reverse
+#' @export
+astr_reverse <- bstr_reverse
+
