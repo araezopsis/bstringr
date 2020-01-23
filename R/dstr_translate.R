e700a86a5b47034a5549b6b581827ce8cf742b93
Toshihiro Arae <aroearue@gmail.com>
2020-01-23 18:14:15 +0900

    modify dstr_translate()

--- R/dstr_translate.R
+++ R/dstr_translate.R
@@ -2,8 +2,11 @@
 #' Translate dna -> protein
 #' @param dstrobj sequence you want to translate
 #' @export
+#' @examples
+#' "atgtga" %>% dstr_translate()
+#'
 dstr_translate <-
-  function(dstrobj){
+  function(dstrobj) {
     dstrobj <- as_dstr(dstrobj) %>% bstr_to_upper()
     at <- attributes(dstrobj)
 
@@ -16,4 +19,14 @@ dstr_translate <-
     attributes(pep) <- at
     as_astr(pep)
   }
+
+# test_orf <-
+#   Biostrings::GENETIC_CODE %>%
+#   {tibble::tibble(aa = ., codon = names(.))} %>%
+#   dplyr::group_by(aa) %>%
+#   dplyr::sample_n(1) %>%
+#   dplyr::pull(codon) %>%
+#   paste(collapse = "")
+# test_orf %>% dstr_translate()
+# test_orf %>% bstr_sub(end = -2) %>% dstr_translate()
 
