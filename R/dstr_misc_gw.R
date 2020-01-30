dbe5d7e361f55a665444d1cc6b2e0cf5e8406cf2
Toshihiro Arae <aroearue@gmail.com>
2020-01-30 14:21:42 +0900

    modify dstr_misc_gw.R

--- R/dstr_misc_gw.R
+++ R/dstr_misc_gw.R
@@ -1,15 +1,50 @@
 
-
 #' TOPO reaction and LR reaction
 #' @param insert insert sequence. without 5'CACC
 #' @param pentr optional. pENTR D-TOPO sequence or the fasta file path
 #' @param entry Entry clone
 #' @param distination Distination vector
+#' @param distination_to_lower change distination vector to lower case
 #' @name gw
 #' @examples
 #' dstr_gw_topo("atgatgatgtga")
 #'
+#' attR1 <- "ACAAGTTTGTACAAAAAAGCTGAAC"
+#' attR2 <- "GTTCAGCTTTCTTGTACAAAGTGGT"
+#' A9 <- "AAAAAAAAA"
+#' dummy <- paste0(A9, attR1, A9, attR2, A9, collapse = "")
+#' dummy2 <- dstr_rev_comp(dummy)
+#' dummy3 <- paste0(dummy, dummy2, collapse = "")
+#'
+#' test_distination <- dstr(c(dummy, dummy2, dummy3), paste0("dummy", 1:3))
+#' test_distination
+#'
+#' dstr_gw_topo("atgtga") %>%
+#'   dstr_gw_lr(test_distination) %>%
+#'   write_fasta()
+#'
+#' dstr_gw_topo("atgtga") %>%
+#'   dstr_gw_lr_both(test_distination) %>%
+#'   write_fasta()
+#'
 NULL
+
+#' @rdname gw
+#' @export
+get_pentr <- function(pentr) {
+  if(missing(pentr)) {
+    pentr_url <-
+      "http://tools.thermofisher.com/content/sfs/vectors/pentr_dtopo_seq.txt"
+    pentr <-
+      readr::read_lines(pentr_url) %>%
+      paste(collapse = "") %>%
+      as_dstr("pENTR D-TOPO")
+  } else {
+    if(file.exists(pentr)) pentr <- read_fasta(pentr) %>% as_dstr()
+    else pentr <- as_dstr(pentr)
+  }
+  pentr
+}
 
 #' @rdname gw
 #' @export
@@ -19,17 +54,7 @@ dstr_gw_topo <- function(insert, pentr) {
   topo_5term <- "AAGGGTGGGCGCGCCGAC"
 
   # Read pENTR D-TOPO sequence
-  if(missing(pentr)) {
-    pentr_url <-
-      "http://tools.thermofisher.com/content/sfs/vectors/pentr_dtopo_seq.txt"
-    pentr <-
-      readr::read_lines(pentr_url) %>%
-      paste(collapse = "") %>%
-      as_dstr()
-  } else {
-    if(file.exists(pentr)) pentr <- read_fasta(pentr) %>% as_dstr()
-    else pentr <- as_dstr(pentr)
-  }
+  pentr <- get_pentr(pentr)
   if(!all(bstr_detect(pentr, topo_3term) & bstr_detect(pentr, topo_5term)))
     stop("pentr does not have valid pENTR D-TOPO sequence.")
 
@@ -51,49 +76,51 @@ dstr_gw_topo <- function(insert, pentr) {
 #' @rdname gw
 #' @export
 dstr_gw_lr <-
-  function(entry, distination){
+  function(entry, distination, distination_to_lower = TRUE){
     . <- NULL
     entry <- as_dstr(entry)
     ne <- names(entry)
     distination <- as_dstr(distination)
     nd <- names(distination)
 
-    attR1 <- "(?i)ACAAGTTTGTACAAAAAAGCTGAAC" #attR1 for distination
-    attR2 <- "(?i)GTTCAGCTTTCTTGTACAAAGTGGT" #attR2 for distination
-    attL1  <- "(?i)CCAACTTTGTACAAAAAAGCAGGCT" #attL1 for entry
-    attL2  <- "(?i)ACCCAGCTTTCTTGTACAAAGTTGG" #attL2 for entry
+    if(distination_to_lower) distination <- bstr_to_lower(distination)
 
-    if(any(!bstr_detect(distination, attR1)))
-      stop("distination have no attR1 sequence.")
-    if(any(!bstr_detect(distination, attR2)))
-      stop("distination have no attR2 sequence.")
-    if(any(!bstr_detect(entry, attL1)))
-      stop("entry have no attL1 sequence.")
-    if(any(!bstr_detect(entry, attL2)))
-      stop("entry have no attL2 sequence.")
+    add_i_mod <- function(x) paste0("(?i)", x)
+    attR1 <- "ACAAGTTTGTACAAAAAAGCTGAAC" #attR1 for distination
+    attR2 <- "GTTCAGCTTTCTTGTACAAAGTGGT" #attR2 for distination
+    pattern_attR12 <- paste0(attR1, ".*?", attR2) %>% add_i_mod
+    attL1 <- "CCAACTTTGTACAAAAAAGCAGGCT" #attL1 for entry
+    attL2 <- "ACCCAGCTTTCTTGTACAAAGTTGG" #attL2 for entry
+    pattern_attL12 <- paste0(attL1, ".*?", attL2) %>% add_i_mod
+    attB1 <- "ACAAGTTTGTACAAAAAAGCAGGCT" #attB1 for product
+    attB2 <- "ACCCAGCTTTCTTGTACAAAGTGGT" #attB2 for product
 
-    recombinate <-
-      paste0(
-        #outer_left
-        stringr::str_to_lower(
-          stringr::str_extract(distination, paste0("^.*", attR1)) %>%
-            {stringr::str_sub(., end = stringr::str_count(.) - 17)}
-        ),
-        #insert
-        stringr::str_to_upper(
-          stringr::str_extract(entry, paste0(attL1, ".*", attL2)) %>%
-            {stringr::str_sub(., start = 9, end = stringr::str_count(.) - 15)}
-        ),
+    loc_attR12 <- bstr_locate(distination, pattern_attR12)
 
-        #outer_right
-        stringr::str_to_lower(
-          stringr::str_extract(distination, paste0(attR2, ".*$")) %>%
-            stringr::str_sub(start = 11)
-        )
+    f <- function(x) {
+      bstr_replace(
+        bstrobj = x,
+        pattern = pattern_attL12,
+        replacement = . %>%
+          bstr_replace(add_i_mod(attL1), attB1) %>%
+          bstr_replace(add_i_mod(attL2), attB2)
       )
-    recombinate %>% dstr(ne)
+    }
+    modified_entry <- entry %>% f()
+    subseq_attB12 <-
+      bstr_extract(modified_entry, add_i_mod(paste0(attB1, ".*?", attB2)))[[1]] %>%
+      bstr_to_upper()
+
+    bstr_sub_all(distination, loc_attR12, omit_na = TRUE) <- subseq_attB12
+    distination
   }
 
+#' @rdname gw
+#' @export
+dstr_gw_lr_both <- function(entry, distination){
+  product <- dstr_gw_lr(entry, distination) %>% dstr_rev_comp()
+  dstr_gw_lr(entry, product, distination_to_lower = FALSE) %>% dstr_rev_comp()
+}
 
 #' @rdname gw
 #' @export
