f7afbba9dd054ff4dcb9d49872bcdc9b3d03dbcd
Toshihiro Arae <aroearue@gmail.com>
2020-06-25 14:08:30 +0900

    Update bstr_reverse.R

--- R/bstr_reverse.R
+++ R/bstr_reverse.R
@@ -7,14 +7,13 @@
 #' temp <- bstr_rand_seq(2, 6, seed = 1)
 #' c(temp, reverse = bstr_reverse(temp))
 #'
-bstr_reverse <-
-  function(bstrobj){
-    bstrobj <- as_bstr(bstrobj)
-    at <- attributes(bstrobj)
+bstr_reverse <- function(bstrobj) {
+  bstrobj <- as_bstr(bstrobj)
+  at <- attributes(bstrobj)
 
-    bstrobj <- stringi::stri_reverse(bstrobj)
+  bstrobj <- stringi::stri_reverse(bstrobj)
 
-    attributes(bstrobj) <- at
-    bstrobj
-  }
+  attributes(bstrobj) <- at
+  bstrobj
+}
 
